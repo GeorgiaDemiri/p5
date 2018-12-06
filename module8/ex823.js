e0703cc9daf7c717a4965acf09ae3528cc4d78a3
georgiademiri <georgiademiri@gmail.com>
2018-12-06 23:14:07 +0200

    Primitives Shapes - line

---
+++ module8/ex823.js
@@ -0,0 +1,20 @@
+function setup() {
+  // create canvas
+  createCanvas(500,500);
+  // set background color
+  }
+ 
+
+function draw() { 
+  background('#859774');
+  noFill();
+  fill ("#1077");
+  ellipse(250, 400, 150, 200);
+  ellipse (500,400,150,200);
+ ellipse (450,450, 600,600);
+ ellipse (400, 600, 200,100);
+ fill ("#695578");
+ ellipse (300,180, 500,100);
+ 
+}
+
